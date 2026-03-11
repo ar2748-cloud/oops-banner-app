20eb8e679d5b80c555d0dfac9017fe50fd53d6a4
ar2748-cloud <ar2748@srmist.edu.in>
2026-03-12 00:50:14 +0530

    Coded UC7 to Print OOPS Banner using Inner Class

--- BannerApp.java
+++ BannerApp.java
@@ -2,42 +2,23 @@ public class BannerApp {
 
     public static void main(String[] args) {
 
-        printLine1();
-        printLine2();
-        printLine3();
-        printLine4();
-        printLine5();
-        printLine6();
-        printLine7();
+        Banner banner = new Banner();
+        banner.printBanner();
 
     }
 
-    public static void printLine1() {
-        System.out.println(" *****   *****   ******   ***** ");
-    }
+    static class Banner {
 
-    public static void printLine2() {
-        System.out.println("*     * *     *  *     * *     *");
-    }
+        public void printBanner() {
 
-    public static void printLine3() {
-        System.out.println("*     * *     *  *     * *");
-    }
+            System.out.println(" *****   *****   ******   ***** ");
+            System.out.println("*     * *     *  *     * *     *");
+            System.out.println("*     * *     *  *     * *");
+            System.out.println("*     * *     *  ******   ***** ");
+            System.out.println("*     * *     *  *             *");
+            System.out.println("*     * *     *  *       *     *");
+            System.out.println(" *****   *****   *        ***** ");
 
-    public static void printLine4() {
-        System.out.println("*     * *     *  ******   *****");
+        }
     }
-
-    public static void printLine5() {
-        System.out.println("*     * *     *  *             *");
-    }
-
-    public static void printLine6() {
-        System.out.println("*     * *     *  *       *     *");
-    }
-
-    public static void printLine7() {
-        System.out.println(" *****   *****   *        ***** ");
-    }
-
 }
