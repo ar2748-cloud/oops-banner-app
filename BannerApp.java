bf9c0c63e237ce2de705baafbad936675f8880fb
ar2748-cloud <ar2748@srmist.edu.in>
2026-03-12 00:14:40 +0530

    Coded UC4 to Print OOPS Banner using Array and Loop

--- BannerApp.java
+++ BannerApp.java
@@ -2,13 +2,27 @@ public class BannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("  *****   *****   ******   ***** ");
-        System.out.println(" *     * *     *  *     * *     *");
-        System.out.println(" *     * *     *  *     * *      ");
-        System.out.println(" *     * *     *  ******   ***** ");
-        System.out.println(" *     * *     *  *             *");
-        System.out.println(" *     * *     *  *       *     *");
-        System.out.println("  *****   *****   *        ***** ");
+        String[] line1 = {" ***** ", " ***** ", " ****** ", " ***** "};
+        String[] line2 = {"*     *", "*     *", "*     *", "*     *"};
+        String[] line3 = {"*     *", "*     *", "*     *", "*"};
+        String[] line4 = {"*     *", "*     *", "******", " ***** "};
+        String[] line5 = {"*     *", "*     *", "*", "*"};
+        String[] line6 = {"*     *", "*     *", "*", "*     *"};
+        String[] line7 = {" ***** ", " ***** ", "*", " ***** "};
 
+        printLine(line1);
+        printLine(line2);
+        printLine(line3);
+        printLine(line4);
+        printLine(line5);
+        printLine(line6);
+        printLine(line7);
+    }
+
+    public static void printLine(String[] line) {
+        for (String part : line) {
+            System.out.print(part + " ");
+        }
+        System.out.println();
     }
 }
